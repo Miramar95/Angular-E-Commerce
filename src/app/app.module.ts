68c1bcaea31cbe7be914845a4ceb921b3b06b83b
Mira <mira.ph.ling@gmail.com>
2019-04-02 17:25:05 +0200

    Creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CartCounterComponent } from './components/cart-counter/cart-counter.component';
+import { WishListComponent } from './components/wish-list/wish-list.component';
+import { SingleProductComponent } from './components/single-product/single-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NotFoundComponent,
+    CartCounterComponent,
+    WishListComponent,
+    SingleProductComponent
   ],
   imports: [
     BrowserModule,
